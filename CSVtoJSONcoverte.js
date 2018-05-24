f51dc994f36db12765f8d05452bbabc9a3d0a6f1
prabhat903 <39587372+prabhat903@users.noreply.github.com>
2018-05-24 17:16:11 +0530

    Add files via upload

---
+++ CSVtoJSONcoverte.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path =  require('path');
+
+const convert = (csvpath) => {
+	const getCSV = (from,callback)=>{
+		console.log(path.join(__dirname,from));
+		fs.readFile(path.join(__dirname,from),{encoding: 'utf-8'},(error,data)=>{
+			if(error) {return callback(error);}
+			var arr = data.split(/\r\n/ig);
+			var keys = arr.shift().split(',');
+			var result = [];
+			console.log(arr.length);
+			for(var k=0; k<arr.length-1;k++){
+				result.push({});
+				arr[k].split(',').forEach((ele,ind)=>{
+					result[k][keys[ind]] = ele;
+				});
+			}
+			console.log(result.length)
+			callback(null,JSON.stringify(result));
+		});
+	};
+	const createJSON = (error,data)=>{
+		if(error) return console.log(error);
+		if(fs.existsSync()){fs.mkdir('JSON_output');}
+		fs.writeFileSync(path.join(__dirname,'./JSON_output/customer-data.json'),data);
+		console.log(`please find JSON in this path ${path.join(__dirname,'./JSON_output')}.`);
+	};
+	getCSV(csvpath,createJSON);
+}
+convert(process.argv[2]);
